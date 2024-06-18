6d702bb5c6b4ba425d265ac7fd3b0546517b6a29
Tom <jtomtan@qq.com>
2024-06-18 16:55:00 +0800

    Create lib.rs

---
+++ ownbit-solana-multisig/src/lib.rs
@@ -0,0 +1,252 @@
+se anchor_lang::prelude::*;
+use anchor_lang::solana_program;
+use anchor_lang::solana_program::instruction::Instruction;
+use std::convert::Into;
+use std::ops::Deref;
+
+declare_id!("JCdDLzhavKvHgGEfBnQQmFDSwFfe1JKW7j7NVgCti5Ny");
+
+// 导入 ErrorCode 宏
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The given owner is not part of this multisig.")]
+    InvalidOwner,
+    #[msg("Owners length must be non zero.")]
+    InvalidOwnersLen,
+    #[msg("Not enough owners signed this transaction.")]
+    NotEnoughSigners,
+    #[msg("Cannot delete a transaction that has been signed by an owner.")]
+    TransactionAlreadySigned,
+    #[msg("Overflow when adding.")]
+    Overflow,
+    #[msg("Cannot delete a transaction the owner did not create.")]
+    UnableToDelete,
+    #[msg("The given transaction has already been executed.")]
+    AlreadyExecuted,
+    #[msg("Threshold must be less than or equal to the number of owners.")]
+    InvalidThreshold,
+    #[msg("Owners must be unique")]
+    UniqueOwners,
+}
+
+
+#[program]
+pub mod ownbit_multisig {
+    use super::*;
+
+    // 初始化一个新的多签账户，并设置一组所有者和阈值。
+    pub fn create_multisig(
+        ctx: Context<CreateMultisig>,
+        owners: Vec<Pubkey>,
+        threshold: u64,
+        nonce: u8,
+    ) -> Result<()> {
+        assert_unique_owners(&owners)?;
+        require!(
+            threshold > 0 && threshold <= owners.len() as u64,
+            ErrorCode::InvalidThreshold
+        );
+        require!(!owners.is_empty(), ErrorCode::InvalidOwnersLen);
+
+        let multisig = &mut ctx.accounts.multisig;
+        multisig.owners = owners;
+        multisig.threshold = threshold;
+        multisig.nonce = nonce;
+        Ok(())
+    }
+
+    // 创建一个新的交易账户，由创建者自动签名，
+    // 创建者必须是多签的所有者之一。
+    pub fn create_transaction(
+        ctx: Context<CreateTransaction>,
+        pid: Pubkey,
+        accs: Vec<TransactionAccount>,
+        data: Vec<u8>,
+    ) -> Result<()> {
+        let owner_index = ctx
+            .accounts
+            .multisig
+            .owners
+            .iter()
+            .position(|a| a == ctx.accounts.proposer.key)
+            .ok_or(ErrorCode::InvalidOwner)?;
+
+        let mut signers = Vec::new();
+        signers.resize(ctx.accounts.multisig.owners.len(), false);
+        signers[owner_index] = true;
+
+        let tx = &mut ctx.accounts.transaction;
+        tx.program_id = pid;
+        tx.accounts = accs;
+        tx.data = data;
+        tx.signers = signers;
+        tx.multisig = ctx.accounts.multisig.key();
+        tx.did_execute = false;
+
+        Ok(())
+    }
+
+    // 为多签的所有者批准一个交易。
+    pub fn approve(ctx: Context<Approve>) -> Result<()> {
+        let owner_index = ctx
+            .accounts
+            .multisig
+            .owners
+            .iter()
+            .position(|a| a == ctx.accounts.owner.key)
+            .ok_or(ErrorCode::InvalidOwner)?;
+
+        ctx.accounts.transaction.signers[owner_index] = true;
+
+        Ok(())
+    }
+  
+    // 如果有足够的签名者，则执行给定的交易。
+    pub fn execute_transaction(ctx: Context<ExecuteTransaction>) -> Result<()> {
+        // 是否已经执行过了？
+        if ctx.accounts.transaction.did_execute {
+            return Err(ErrorCode::AlreadyExecuted.into());
+        }
+
+        // 我们是否有足够的签名者。
+        let sig_count = ctx
+            .accounts
+            .transaction
+            .signers
+            .iter()
+            .filter(|&did_sign| *did_sign)
+            .count() as u64;
+        if sig_count < ctx.accounts.multisig.threshold {
+            return Err(ErrorCode::NotEnoughSigners.into());
+        }
+
+        // 执行交易。
+        let mut ix: Instruction = (*ctx.accounts.transaction).deref().into();
+        ix.accounts = ix
+            .accounts
+            .iter()
+            .map(|acc| {
+                let mut acc = acc.clone();
+                if &acc.pubkey == ctx.accounts.multisig_signer.key {
+                    acc.is_signer = true;
+                }
+                acc
+            })
+            .collect();
+        let multisig_key = ctx.accounts.multisig.key();
+        let seeds = &[multisig_key.as_ref(), &[ctx.accounts.multisig.nonce]];
+        let signer = &[&seeds[..]];
+        let accounts = ctx.remaining_accounts;
+        solana_program::program::invoke_signed(&ix, accounts, signer)?;
+
+        // 销毁交易以确保一次性使用。
+        ctx.accounts.transaction.did_execute = true;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct CreateMultisig<'info> {
+    #[account(zero, signer)]
+    multisig: Box<Account<'info, Multisig>>,
+}
+
+#[derive(Accounts)]
+pub struct CreateTransaction<'info> {
+    multisig: Box<Account<'info, Multisig>>,
+    #[account(zero, signer)]
+    transaction: Box<Account<'info, Transaction>>,
+    // One of the owners. Checked in the handler.
+    proposer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Approve<'info> {
+    multisig: Box<Account<'info, Multisig>>,
+    #[account(mut, has_one = multisig)]
+    transaction: Box<Account<'info, Transaction>>,
+    // One of the multisig owners. Checked in the handler.
+    owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ExecuteTransaction<'info> {
+    multisig: Box<Account<'info, Multisig>>,
+    /// CHECK: multisig_signer is a PDA program signer. Data is never read or written to
+    #[account(
+        seeds = [multisig.key().as_ref()],
+        bump = multisig.nonce,
+    )]
+    multisig_signer: UncheckedAccount<'info>,
+    #[account(mut, has_one = multisig)]
+    transaction: Box<Account<'info, Transaction>>,
+}
+
+#[account]
+pub struct Multisig {
+    pub owners: Vec<Pubkey>,
+    pub threshold: u64,
+    pub nonce: u8,
+}
+#[account]
+pub struct Transaction {
+    // The multisig account this transaction belongs to.
+    pub multisig: Pubkey,
+    // Target program to execute against.
+    pub program_id: Pubkey,
+    // Accounts requried for the transaction.
+    pub accounts: Vec<TransactionAccount>,
+    // Instruction data for the transaction.
+    pub data: Vec<u8>,
+    // signers[index] is true iff multisig.owners[index] signed the transaction.
+    pub signers: Vec<bool>,
+    // Boolean ensuring one time execution.
+    pub did_execute: bool,
+}
+
+impl From<&Transaction> for Instruction {
+    fn from(tx: &Transaction) -> Instruction {
+        Instruction {
+            program_id: tx.program_id,
+            accounts: tx.accounts.iter().map(Into::into).collect(),
+            data: tx.data.clone(),
+        }
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct TransactionAccount {
+    pub pubkey: Pubkey,
+    pub is_signer: bool,
+    pub is_writable: bool,
+}
+
+impl From<&TransactionAccount> for AccountMeta {
+    fn from(account: &TransactionAccount) -> AccountMeta {
+        match account.is_writable {
+            false => AccountMeta::new_readonly(account.pubkey, account.is_signer),
+            true => AccountMeta::new(account.pubkey, account.is_signer),
+        }
+    }
+}
+
+impl From<&AccountMeta> for TransactionAccount {
+    fn from(account_meta: &AccountMeta) -> TransactionAccount {
+        TransactionAccount {
+            pubkey: account_meta.pubkey,
+            is_signer: account_meta.is_signer,
+            is_writable: account_meta.is_writable,
+        }
+    }
+}
+
+fn assert_unique_owners(owners: &[Pubkey]) -> Result<()> {
+    for (i, owner) in owners.iter().enumerate() {
+        require!(
+            !owners.iter().skip(i + 1).any(|item| item == owner),
+            ErrorCode::UniqueOwners
+        )
+    }
+    Ok(())
+}
